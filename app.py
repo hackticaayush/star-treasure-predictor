d1b96df24482d6cda6de22f29cbd3ee6fea00027
Aayush <155801761+hackticaayush@users.noreply.github.com>
2026-05-22 11:27:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # ── CONFIG ────────────────────────────────────────────────────────────────────
 DATA_FILE              = "round_data.json"
 SKIP_TOP1_THRESHOLD    = 0.180
-SKIP_ENTROPY_THRESHOLD = 2.70
+SKIP_ENTROPY_THRESHOLD = 2.65
 BRAKE_TRIGGER          = 2
 BRAKE_PAUSE            = 2
 POLL_INTERVAL          = 5
